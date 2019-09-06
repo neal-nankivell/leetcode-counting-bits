a462a6ddc974dca7403c913d8bbc0eced5c0ddd3
neal.nankivell <neal.nankivell@gmail.com>
2019-09-06 13:16:03 +0200

    Implemented solution that loops over the range, calculating the hamming weight each time using this technique
    http://graphics.stanford.edu/~seander/bithacks.html#CountBitsSetParallel
    
    Given we are calculating the hamming weight in sequence I assume there
    is a relationship between i and i-1 so some there must be a more
    efficient solution using a dynamic programming style solution.
    
    The other one to consider would be building a lookup table if the
    range of n is known and not too large. Even if this is not reasonable
    for the larger values of N, precalculating the first x in a lookup table
    would most likely pay off as we are always starting from 0.

--- solution.py
+++ solution.py
@@ -1,3 +1,14 @@
+# Given a non negative integer number num.
+# For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in
+# their binary representation and return them as an array.
+
+
 class Solution:
-    def foo(self) -> str:
-        return "bar"
+    def countBits(self, num: int):
+        return [self.hammingWeight(n) for n in range(num + 1)]
+
+    def hammingWeight(x):
+        x -= (x >> 1) & 0x5555555555555555
+        x = (x & 0x3333333333333333) + ((x >> 2) & 0x3333333333333333)
+        x = (x + (x >> 4)) & 0x0f0f0f0f0f0f0f0f
+        return ((x * 0x0101010101010101) & 0xffffffffffffffff) >> 56
--- test_solution.py
+++ test_solution.py
@@ -6,8 +6,18 @@ class Test_TestSolution(unittest.TestCase):
     def setUp(self):
         self.sut = solution.Solution()
 
-    def test_foo(self):
-        self.assertEqual(self.sut.foo(), "bar")
+    def test_countBits(self):
+        self.assertEqual(
+            self.sut.countBits(1),
+            [0, 1])
+
+        self.assertEqual(
+            self.sut.countBits(2),
+            [0, 1, 1])
+
+        self.assertEqual(
+            self.sut.countBits(5),
+            [0, 1, 1, 2, 1, 2])
 
 
 if __name__ == '__main__':
